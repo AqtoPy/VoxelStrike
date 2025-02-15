725bced7157299993572fa968922c4871006c10a
AqtoPy <rekikreki66@gmail.com>
2025-02-15 23:28:21 +0300

    Update MainMenu.gd

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,40 +1,46 @@
 extends Control
 
 @onready var name_input = $VBoxContainer/NameInput
+@onready var map_selector = $VBoxContainer/MapSelector
+@onready var mode_selector = $VBoxContainer/ModeSelector
 @onready var error_label = $VBoxContainer/ErrorLabel
 
+func _ready():
+    # Инициализация списка карт
+    map_selector.add_item("Карьеры", "res://maps/quarry.tscn")
+    map_selector.add_item("Город", "res://maps/city.tscn")
+    map_selector.add_item("Бункер", "res://maps/bunker.tscn")
+    
+    # Инициализация режимов
+    mode_selector.add_item("Все против всех")
+    mode_selector.add_item("Командный бой")
+    mode_selector.add_item("Захват флага")
+
 func _on_host_pressed():
-    if validate_name():
+    if validate_inputs():
         Global.player_name = name_input.text
-        start_host()
+        Global.check_developer_status()
+        GameManager.start_host(
+            map_selector.get_selected_metadata(),
+            mode_selector.selected
+        )
+        hide()
 
 func _on_join_pressed():
     if validate_name():
         Global.player_name = name_input.text
-        start_client()
+        # ... подключение клиента
 
-func validate_name() -> bool:
-    var name = name_input.text.strip_edges()
-    
-    if name == "AutoAuto":
-        error_label.text = "This name is reserved!"
+func validate_inputs() -> bool:
+    if !validate_name():
         return false
         
-    if name.length() < 3 || name.length() > 16:
-        error_label.text = "Name must be 3-16 characters"
+    if map_selector.selected == -1:
+        error_label.text = "Выберите карту!"
+        return false
+        
+    if mode_selector.selected == -1:
+        error_label.text = "Выберите режим!"
         return false
         
     return true
-
-func start_host():
-    var peer = ENetMultiplayerPeer.new()
-    peer.create_server(PORT)
-    multiplayer.multiplayer_peer = peer
-    hide()
-    # Дополнительная логика инициализации уровня
-
-func start_client():
-    var peer = ENetMultiplayerPeer.new()
-    peer.create_client("localhost", PORT)
-    multiplayer.multiplayer_peer = peer
-    hide()
