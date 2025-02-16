7aa863b4ff3c7e7848f0d87057495c8fda56bd6c
AqtoPy <rekikreki66@gmail.com>
2025-02-16 12:55:07 +0300

    Update UIManager.gd

--- UIManager.gd
+++ UIManager.gd
@@ -1,16 +1,28 @@
 extends CanvasLayer
 
 @onready var player_list = $PlayerList
+@onready var scoreboard = $Scoreboard
+
+func _ready():
+    GameManager.player_list_updated.connect(update_player_list)
 
 func update_player_list():
-    var players = get_tree().get_nodes_in_group("players")
     player_list.clear()
-    
-    for player in players:
-        var entry = "%s: %d Kills" % [player.player_name, player.kills]
+    for player in GameManager.players.values():
+        var entry = "%s [%d]" % [player.player_name, player.kills]
         if player.is_developer:
-            entry += " [DEV]"
+            entry += " ★"
+        if player.name == str(multiplayer.get_unique_id()):
+            entry += " (You)"
         player_list.add_item(entry)
 
-func _process(delta):
-    update_player_list()
+func update_scoreboard():
+    var scores = []
+    for player in GameManager.players.values():
+        scores.append({"name": player.name, "kills": player.kills})
+    
+    scores.sort_custom(func(a, b): return a.kills > b.kills)
+    
+    scoreboard.clear()
+    for entry in scores:
+        scoreboard.add_item("%s: %d kills" % [entry.name, entry.kills])
